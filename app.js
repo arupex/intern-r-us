4285b620abda62bf0a884798a5df67cb1ce137d8
Dan Irwin <dirwin517@gmail.com>
2019-10-04 11:50:53 -0400

    init

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 80;
+
+const bodyParser = require('body-parser');
+
+const querystring = require('querystring');
+
+const fs = require('fs');
+
+app.use(express.static('www'));
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.post('/intern', (req, res) => {
+
+    let body = JSON.parse(JSON.stringify(req.body));
+    console.log('req.body', body);
+    fs.writeFileSync(`./interns/${body.name}-${new Date().getTime()}.json`, JSON.stringify(body, null, 3) , 'utf8');
+
+    res.send('Thank you for applying! Hopefully you will hear from us soon!')
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
